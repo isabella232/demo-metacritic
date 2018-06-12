a64c4518c038ab5d34fcee55a293e897fa3c8881
Julien Paroche <julien.paroche@algolia.com>
2018-06-12 11:05:43 -0700

    Update images, add 4 indices with components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import logo from './images/metacritic.png'
 import './App.css'
 
-import { InstantSearch, SearchBox, Hits, Highlight } from 'react-instantsearch/dom'
+import { InstantSearch, Index, SearchBox, Hits, Highlight } from 'react-instantsearch/dom'
 
 class App extends Component {
   render() {
@@ -10,11 +10,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Metacritic search by Algolia</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
 
         <InstantSearch
           appId="6KK7AINE52"
@@ -22,17 +19,51 @@ class App extends Component {
           indexName="dev_MUSIC"
         >
           <SearchBox />
-          <Hits hitComponent={Item} />
+          <div className="results">
+            <Index indexName="dev_MUSIC">
+              <Hits hitComponent={ItemMusic} />
+            </Index>
+            <Index indexName="dev_TV">
+              <Hits hitComponent={ItemTv} />
+            </Index>
+            <Index indexName="dev_MOVIES">
+              <Hits hitComponent={ItemMovie} />
+            </Index>
+            <Index indexName="dev_GAMES">
+              <Hits hitComponent={ItemGame} />
+            </Index>
+          </div>
         </InstantSearch>
       </div>
     )
   }
 }
 
-const Item = ({ hit }) =>
+const ItemMusic = ({ hit }) =>
   <div className="item">
     <div><Highlight attribute="name" hit={hit} /></div>
     <div><Highlight attribute="artist" hit={hit} /></div>
+    <div>{hit.meta_score}</div>
+  </div>
+
+const ItemTv = ({ hit }) =>
+  <div className="item">
+    <div><Highlight attribute="name" hit={hit} /></div>
+    <div><Highlight attribute="starring" hit={hit} /></div>
+    <div>{hit.meta_score}</div>
+  </div>
+
+const ItemMovie = ({ hit }) =>
+  <div className="item">
+    <div><Highlight attribute="name" hit={hit} /></div>
+    <div><Highlight attribute="genre" hit={hit} /></div>
+    <div>{hit.meta_score}</div>
+  </div>
+
+const ItemGame = ({ hit }) =>
+  <div className="item">
+    <div><Highlight attribute="name" hit={hit} /></div>
+    <div><Highlight attribute="platform" hit={hit} /></div>
     <div>{hit.meta_score}</div>
   </div>
 
